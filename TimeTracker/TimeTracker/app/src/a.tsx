17628c2185b127c9592f7aca9e9b83a642700708
Mantas Damijonaitis <m.damijonaitis@gmail.com>
2017-09-18 20:27:45 +0300

    configured react to multiple entry / output points

---
+++ TimeTracker/TimeTracker/app/src/a.tsx
@@ -0,0 +1,7 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>Hello world a</div>,
+    document.getElementById("example")
+);
---
+++ TimeTracker/TimeTracker/app/src/b.tsx
@@ -0,0 +1,7 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>Hello world b</div>,
+    document.getElementById("example")
+);
