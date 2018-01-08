619fe2f31ee72221a9d134c24e9ca04fd3c457b5
Aleksas Golubevas <aleksas.golubevas@gmail.com>
2018-01-08 20:39:51 +0200

    1.

--- TimeTracker/TimeTracker/app/src/a.tsx
+++ TimeTracker/TimeTracker/app/src/a.tsx
@@ -1,9 +1,12 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 import SampleComponent from "./sample-component";
+import { Timer } from "./models/timer";
+
+declare var myData: Timer[];
 
 ReactDOM.render(
     //<div>Hello world a</div>,
-    <SampleComponent name="mantas" />,
+    <SampleComponent name="OKEY" timers={myData} />,
     document.getElementById("main")
 );
--- TimeTracker/TimeTracker/app/src/sample-component.tsx
+++ TimeTracker/TimeTracker/app/src/sample-component.tsx
@@ -1,14 +1,17 @@
 ﻿import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Timer } from "./models/timer";
 
 export interface ISampleComponentProps 
 {
     name: string;
+    timers: Timer[]
 }
 
 export interface ISampleComponentState
 {
     counter: number;
+    timers: Timer[];
 }
 
 export default class SampleComponent extends React.Component<ISampleComponentProps, ISampleComponentState>
@@ -16,18 +19,23 @@ export default class SampleComponent extends React.Component<ISampleComponentPro
     constructor(props: ISampleComponentProps)
     {
         super(props);
-
+        console.log("props", props);
         this.state = {
             counter: 0,
-
+            timers: props.timers
         };
     }
 
     private addToCounter() : any
     {
+        let oldTimers = this.state.timers;
+        let timer: Timer = {};
+        timer.description = "x";
+        oldTimers.push(timer);
         this.setState({
             counter: this.state.counter + 1,
-        })
+            timers: oldTimers
+        });
     }
 
     render()
@@ -36,6 +44,15 @@ export default class SampleComponent extends React.Component<ISampleComponentPro
             <div>
                 <div>{this.props.name}</div>
                 <button onClick={this.addToCounter.bind(this)}>PRESS ME-{this.state.counter}</button>
+                <table className="table table-bordered table-responsive">
+                    <tbody>
+                        {this.props.timers.map((t, i) => {
+                            return <tr key={i}>
+                                        {t.description}
+                                    </tr>
+                        })}
+                    </tbody>
+                </table>
             </div>
             );
     }
